62a5f209cac690186ed7b7d076acb393fe767a94
bryan.c <forcyever@hotmail.com>
2021-07-02 10:42:52 +0800

    modify

--- common/src/main/java/leetcode/addtwonumbers/AddTwoNumbers.java
+++ common/src/main/java/leetcode/addtwonumbers/AddTwoNumbers.java
@@ -4,7 +4,7 @@ package leetcode.addtwonumbers;
  * @Author:bryan.c
  * @Date:2020/11/19
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
- *
+ *https://leetcode-cn.com/problems/add-two-numbers/
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  *
  * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
@@ -23,31 +23,36 @@ public class AddTwoNumbers {
         ListNode l2=new ListNode(5);
         l2.next=new ListNode(6);
         l2.next.next=new ListNode(4);
-        System.out.println(addTwoNumbers(l1,l2));
+        System.out.println(l1.toString());
+        System.out.println(l2.toString());
+        System.out.println(addTwoNumbers(l1,l2).toString());
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);
-        ListNode cur = pre;
+        ListNode head = null, tail = null;
         int carry = 0;
-        while(l1 != null || l2 != null) {
+        while (l1 != null || l2 != null) {
             int x = l1 == null ? 0 : l1.val;
             int y = l2 == null ? 0 : l2.val;
             int sum = x + y + carry;
 
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
             carry = sum / 10;
-            sum = sum % 10;
-            cur.next = new ListNode(sum);
-
-            cur = cur.next;
-            if(l1 != null)
+            if (l1 != null) {
                 l1 = l1.next;
-            if(l2 != null)
+            }
+            if (l2 != null) {
                 l2 = l2.next;
+            }
         }
-        if(carry == 1) {
-            cur.next = new ListNode(carry);
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
         }
-        return pre.next;
+        return head;
     }
 }
--- common/src/main/java/leetcode/addtwonumbers/ListNode.java
+++ common/src/main/java/leetcode/addtwonumbers/ListNode.java
@@ -4,10 +4,31 @@ package leetcode.addtwonumbers;
  * @Author:bryan.c
  * @Date:2020/11/19
  */
-public class ListNode {
+public class ListNode extends Object {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder=new StringBuilder("[");
+        ListNode cur=this;
+        while (cur.next!=null){
+            stringBuilder.append(cur.val).append(",");
+            cur=cur.next;
+        }
+        stringBuilder.append(cur.val).append("]");
+        return stringBuilder.toString();
+    }
 }
